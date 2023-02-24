c2d83067a340c592067f1699d7a7b84670795110
teto-soft <48591301+teto-soft@users.noreply.github.com>
2023-02-25 01:09:44 +0900

    about ページ作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => (
+  <main>
+    <h1>About Me</h1>
+    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+  </main>
+)
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
