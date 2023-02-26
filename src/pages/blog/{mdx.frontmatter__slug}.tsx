f557e71b26374334a96bf22e7d4ef8ef9ac9c028
teto-soft <48591301+teto-soft@users.noreply.github.com>
2023-02-27 02:21:12 +0900

    画像を表示するよう実装

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,27 +1,47 @@
 import { graphql, PageProps } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import Seo from '../../components/Seo'
 
-const BlogPost = ({ data, children }: PageProps<Queries.BlogPostQuery>) => (
-  <Layout pageTitle={data.mdx?.frontmatter?.title || ''}>
-    <p>{data.mdx?.frontmatter?.date}</p>
-    {children}
-  </Layout>
-)
-console.log(BlogPost)
+const BlogPost = ({ data, children }: PageProps<Queries.BlogPostQuery>) => {
+  const frontmatter = data.mdx?.frontmatter
+  const image = frontmatter?.hero_image?.childImageSharp?.gatsbyImageData
+  const imageCreditLink = frontmatter?.hero_image_credit_link
 
-export const Head = ({ data }: PageProps<Queries.BlogPostQuery>) => <Seo title={data.mdx?.frontmatter?.title || ''} />
-
-export default BlogPost
+  return (
+    <Layout pageTitle={data.mdx?.frontmatter?.title || ''}>
+      <p>{data.mdx?.frontmatter?.date}</p>
+      {image && <GatsbyImage image={image} alt={frontmatter?.hero_image_alt || ''} />}
+      {imageCreditLink && (
+        <p>
+          Photo Credit: <a href={imageCreditLink || ''}>{frontmatter?.hero_image_credit_text}</a>
+        </p>
+      )}
+      {children}
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query BlogPost($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
 `
+
+export const Head = ({ data }: PageProps<Queries.BlogPostQuery>) => <Seo title={data.mdx?.frontmatter?.title || ''} />
+
+export default BlogPost
