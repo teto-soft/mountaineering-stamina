17f5cab8530e9b83594aa23187b6d664e9d54d80
teto-soft <48591301+teto-soft@users.noreply.github.com>
2023-02-26 22:40:46 +0900

    ブログページで mdx を使用するよう変更

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,19 +5,26 @@ import Seo from '../components/Seo'
 
 const BlogPage = ({ data }: PageProps<Queries.BlogPageQuery>) => (
   <Layout pageTitle='My Blog Posts'>
-    <ul>
-      {data.allFile.nodes.map((node) => (
-        <li key={node.name}>{node.name}</li>
-      ))}
-    </ul>
+    {data.allMdx.nodes.map((node) => (
+      <article key={node.id}>
+        <h2>{node.frontmatter?.title}</h2>
+        <p>Posted: {node.frontmatter?.date}</p>
+        <p>{node.excerpt}</p>
+      </article>
+    ))}
   </Layout>
 )
 
 export const query = graphql`
   query BlogPage {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
